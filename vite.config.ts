e1ad95eedb37df4e156a93dabefedbe5c00d61ea
alikia2x <alikia2x@outlook.com>
2024-08-13 17:07:41 +0800

    update: package

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: "src/index.ts",
+			name: "zxcvbn",
+			fileName: "index"
+		},
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "src/index.ts")
+			},
+			output: {
+				exports: "named"
+			}
+		}
+	},
+	plugins: [dts()]
+});
